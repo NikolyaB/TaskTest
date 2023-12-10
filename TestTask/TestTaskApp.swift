146b6816b5531a062a5904db3daa2a73a3b74f58
Nikolay Bocharnikov <nikolay.bocharnikov@sebbia.com>
2023-12-10 12:44:25 +0300

    Initial Commit

---
+++ TestTask/TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskApp.swift
+//  TestTask
+//
+//  Created by Nikolay Bocharnikov on 10.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
